963abde28ab9bb6365aecbffdf0546b6c4f99bfd
satheeshkumarkn <satheesh.kumarkn@365smartshop.com>
2025-02-23 15:40:57 +0530

    Create setup-hooks.sh

---
+++ setup-hooks.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Setting up Git hooks..."
+
+# Move the post-checkout hook script into the .git/hooks directory
+mv scripts/hooks/post-checkout .git/hooks/post-checkout
+
+# Make the post-checkout hook script executable
+chmod +x .git/hooks/post-checkout
+
+# Move the commit-msg hook script into the .git/hooks directory
+mv commit-msg .git/hooks/commit-msg
+
+# Make the commit-msg hook script executable
+chmod +x .git/hooks/commit-msg
+
+echo "Git hooks setup completed."
